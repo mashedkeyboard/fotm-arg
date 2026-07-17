73989d18f6964083492a41c6d3de39ef7c3c4711
cpf <16772519+mashedkeyboard@users.noreply.github.com>
2026-07-17 13:32:09 +0100

    force nbsp

--- src/routes/(authed)/redeem/[uuid]/+page.svelte
+++ src/routes/(authed)/redeem/[uuid]/+page.svelte
@@ -10,7 +10,7 @@
 </svelte:head>
 
 <h1>{data.tagTitle}</h1>
-<h4>(by {data.author.name}{#if data.author.phone} on DECT {data.author.phone}{/if})</h4>
+<h4>(by {data.author.name}{#if data.author.phone}&nbsp;on DECT {data.author.phone}{/if})</h4>
 <p>You found a moontag! There's more hidden around site...</p>
 <p>Claim them all for a stamp!</p>
 <a href={resolve("/help")}>give me my next hint</a>
